241d96ee5e132a29c19c56a447fd9f8294e43b98
Nicolas Wladimir Adasme Arroyo <nicolas.adasme1701@alumnos.ubiobio.cl>
2019-09-16 01:50:24 -0300

    Version 2

--- Modelo/usuarios_model.php
+++ Modelo/usuarios_model.php
@@ -1,5 +1,4 @@
 <?php
-
     class usuarios_model{
 
         private $db;
@@ -7,23 +6,36 @@
 
         public function __construct(){
 
-            require_once("conexion.php");
+            try{
+                require_once("conexion.php");
 
-            $this->db= Conectar::Conexion();
+                $this->db= Conectar::Conexion();
 
-            $this->usuario= array();
+                $this->usuario= array();
+
+            }catch(Exception $e){
+                echo "La Línea de error es: ". $e->getLine();
+            }
+            
         }
 
         public function get_usuarios(){
 
             // PDO
-            $query= $this->db->query("SELECT * FROM usuarios");
+            try{
+                $query= $this->db->query("SELECT * FROM usuarios");
 
-            while($fila= $query->fetch(PDO::FETCH_ASSOC)){
-                $this->usuario[]= $fila;
+                while($fila= $query->fetch(PDO::FETCH_ASSOC)){
+                    $this->usuario[]= $fila;
+                }
+
+                $query->closeCursor();
+
+                return $this->usuario;
+
+            }catch(Exception $e){
+                echo "La Línea de error es: ". $e->getLine();
             }
-
-            return $this->usuario;
 
             /* MYSQLI
             $result= $this->conexion->query('SELECT * FROM usuarios');
@@ -33,64 +45,108 @@
 
         public function agregar_usuarios($datos){
 
-            $sql= "INSERT INTO usuarios (Nombre,Apellido,Correo,Contra) VALUES (:nombre,:apellido,:correo,:contra)";
+            try{
+                $sql= "INSERT INTO usuarios (Nombre,Apellido,Correo,Contra) VALUES (:nombre,:apellido,:correo,:contra)";
             
-            $query= $this->db->prepare($sql);
+                $query= $this->db->prepare($sql);
 
-            $array= array(":nombre"=>$datos[0],
-                            ":apellido"=>$datos[1],
-                            ":correo"=>$datos[2],
-                            ":contra"=>$datos[3]);
+                /*$array= array(":nombre"=>$datos[0],
+                                ":apellido"=>$datos[1],
+                                ":correo"=>$datos[2],
+                                ":contra"=>$datos[3]);*/
 
-            $query->execute($array);
-            
-            //$query= $this->db->query($sql);
+                $query->bindParam(":nombre", $datos[0], PDO::PARAM_STR);
+                $query->bindParam(":apellido", $datos[1], PDO::PARAM_STR);
+                $query->bindParam(":correo", $datos[2], PDO::PARAM_STR);
+                $query->bindParam(":contra", $datos[3], PDO::PARAM_STR);
 
-            return $query;
+                $query->execute();
+                
+                //$query= $this->db->query($sql);
+
+                return $query;
+
+            }catch(Exception $e){
+                echo "La Línea de error es: ". $e->getLine();
+            }
         }
 
         public function eliminar_usuarios($dato){
 
-            $sql= "DELETE FROM usuarios WHERE ID= $dato "; // ID= :dato
+            try{
+                $sql= "DELETE FROM usuarios WHERE ID= :dato "; // ID= :dato
 
-            return $this->db->query($sql);
+                //$this->db->execute();
 
-            /*$this->db->prepare($sql);
+                //return $this->db->query($sql);
 
-            $this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                $consulta= $this->db->prepare($sql);
 
-            $this->db->setFetchMode(PDO::FETCH_OBJ);
+                $this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-            $this->db->bindParam(":dato", $dato, PDO::PARAM_INT);
+                $consulta->bindParam(":dato", $dato, PDO::PARAM_INT);
 
-            $this->db->execute();*/
+                $query= $consulta->execute();
+
+                return $query;
+
+            }catch(Exception $e){
+                echo "La Línea de error es: ". $e->getLine();
+            }
         }
 
         public function obtener_usuario($dato){
 
-            $query= $this->db->query("SELECT * FROM usuarios WHERE ID=$dato");
+            try{
+                $consulta= $this->db->prepare("SELECT * FROM usuarios WHERE ID=:dato");
 
-            while($fila= $query->fetch(PDO::FETCH_ASSOC)){
-                $this->usuario[]= $fila;
+                $consulta->bindParam(":dato", $dato, PDO::PARAM_INT);
+
+                $consulta->execute();
+
+                while($fila= $consulta->fetch(PDO::FETCH_ASSOC)){
+                    $this->usuario[]= $fila;
+                }
+
+                $consulta->closeCursor();
+
+                return $this->usuario;
+
+            }catch(Exception $e){
+                echo "La Línea de error es: ". $e->getLine();
             }
 
-            return $this->usuario;
         }
 
         public function modificar_usuario($dato){
 
-            $sql= "UPDATE usuarios SET  
-                    Nombre= '$dato[1]', 
-                    Apellido= '$dato[2]', 
-                    Correo= '$dato[3]', 
-                    Contra= '$dato[4]' 
-                    WHERE ID= '$dato[0]'";
+            try{
+                $sql= "UPDATE usuarios SET  
+                        Nombre= :nombre, 
+                        Apellido= :apellido, 
+                        Correo= :correo, 
+                        Contra= :contra 
+                        WHERE ID= :id ";
 
-            return $this->db->query($sql);
+                //return $this->db->query($sql);
+
+                $consulta= $this->db->prepare($sql);
+
+                $consulta->bindParam(":nombre", $dato[1], PDO::PARAM_STR);
+                $consulta->bindParam(":apellido", $dato[2], PDO::PARAM_STR);
+                $consulta->bindParam(":correo", $dato[3], PDO::PARAM_STR);
+                $consulta->bindParam(":contra", $dato[4], PDO::PARAM_STR);
+                $consulta->bindParam(":id", $dato[0], PDO::PARAM_INT);
+
+                $consulta->execute();
+
+                return $consulta;
+
+            }catch(Exception $e){
+                echo "La Línea de error es: ". $e->getLine();
+            }
+
         }
     }
-
-
-
 
 ?>
